745867934fead44364bd8be6fc07e3593694ced0
jfrasinska <j.frasinska@gmail.com>
2023-12-03 21:40:32 +0100

    adding first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import React, { useState } from 'react';
+import OpinionFeedback from './OpinionFeedback';
+
+const App = () => {
+  const [feedbackStats, setFeedbackStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = opinion => {
+    setFeedbackStats(prevStats => ({
+      ...prevStats,
+      [opinion]: prevStats[opinion] + 1,
+    }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Feedback App</h1>
+      <OpinionFeedback opinion="good" onFeedback={handleFeedback} />
+      <OpinionFeedback opinion="neutral" onFeedback={handleFeedback} />
+      <OpinionFeedback opinion="bad" onFeedback={handleFeedback} />
+
+      <h2>Feedback Statistics</h2>
+      <p>Good: {feedbackStats.good}</p>
+      <p>Neutral: {feedbackStats.neutral}</p>
+      <p>Bad: {feedbackStats.bad}</p>
     </div>
   );
 };
+
+export default App;
